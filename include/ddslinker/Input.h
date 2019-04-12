d0dd3af370041c0c59c05e3a06d4454f61252c86
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2019-04-12 21:37:43 +0000

    removed yarp and fixed appbase dependancy

--- include/ddslinker/Input.h
+++ include/ddslinker/Input.h
@@ -2,31 +2,13 @@
 
 #include <string>
 #include <functional>
-#include <yarp/os/PortReader.h>
-#include <yarp/os/Bottle.h>
-#include <yarp/os/ConnectionReader.h>
 
 #include "../../src/Base.h"
 
-using yarp::os::Bottle;
-using yarp::os::ConnectionReader;
-using yarp::os::PortReader;
 using namespace std;
 
 namespace Friend {
 namespace DDS {
-
-class YarpReaderDataProcessor : public PortReader {
-public:
-    typedef std::function<void(ConnectionReader&)> readCallback;
-    virtual void setCallback(const readCallback& callback) { _callback = callback; }
-    bool read(ConnectionReader& data) override {
-        _callback(data);
-        return true;
-    }
-private:
-    readCallback _callback;
-};
 
 template <typename T>
 class Input : public Friend::DDS::Base<T> {
@@ -39,7 +21,6 @@ public:
     virtual void create(const string& name, const readCallback& callback);
     bool isConnected() override { return false; };
 private:
-    YarpReaderDataProcessor _dataProcessor;
     T _lastState;
     readCallback _callback;
 
