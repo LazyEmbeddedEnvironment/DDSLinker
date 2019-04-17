88fba0d2baa62b336a9e787b876fd51ec0a1349a
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2019-04-17 21:01:04 +0000

    compiling all tests and running

--- test/src/Base_test.cpp
+++ test/src/Base_test.cpp
@@ -4,54 +4,54 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-//#include <memory>
-//
-//#define private public
-//#define protected public
-//#include "../../src/Base.h"
-//
-//const string name = "someName";
-//const string expectedName = "int.someName";
-//
-//class BaseInheritor : public Friend::DDS::Base<int>
-//{
-//public:
-//    bool isConnected() override {};
-//    using Friend::DDS::Base<int>::Base;
-//    ~BaseInheritor() { };
-//};
-//
-//class MockBaseInheritor : public BaseInheritor
-//{
-//public:
-//    MOCK_METHOD1(create, void(const string& name));
-//};
-//
-//class BaseTest : public ::testing::Test
-//{
-//public:
-//    unique_ptr<BaseInheritor> base;
-//    void SetUp() override {
-//        base = unique_ptr<BaseInheritor>(new BaseInheritor());
-//    }
-//    void TearDown() override {
-//
-//    }
-//};
-//
-//TEST_F(BaseTest, classCreated) {
-//    ASSERT_TRUE(base != nullptr);
-//}
-//TEST_F(BaseTest, setName) {
-//    base->create(name);
-//    ASSERT_EQ(expectedName, base->_myName);
-//}
-//TEST_F(BaseTest, readLastState) {
-//    base->_lastState = 42;
-//    ASSERT_EQ(42, base->read());
-//}
-//TEST_F(BaseTest, DISABLED_createClassWithName) {
-//    unique_ptr<MockBaseInheritor> mockBase;
-//    EXPECT_CALL(*mockBase, create(expectedName));
-//    mockBase = unique_ptr<MockBaseInheritor>(new MockBaseInheritor());
-//}
+#include <memory>
+
+#define private public
+#define protected public
+#include "../../src/Base.h"
+
+const string name = "someName";
+const string expectedName = "int.someName";
+
+class BaseInheritor : public Friend::DDS::Base<int>
+{
+public:
+    bool isConnected() override {};
+    using Friend::DDS::Base<int>::Base;
+    ~BaseInheritor() { };
+};
+
+class MockBaseInheritor : public BaseInheritor
+{
+public:
+    MOCK_METHOD1(create, void(const string& name));
+};
+
+class BaseTest : public ::testing::Test
+{
+public:
+    unique_ptr<BaseInheritor> base;
+    void SetUp() override {
+        base = unique_ptr<BaseInheritor>(new BaseInheritor());
+    }
+    void TearDown() override {
+
+    }
+};
+
+TEST_F(BaseTest, classCreated) {
+    ASSERT_TRUE(base != nullptr);
+}
+TEST_F(BaseTest, setName) {
+    base->create(name);
+    ASSERT_EQ(expectedName, base->_myName);
+}
+TEST_F(BaseTest, readLastState) {
+    base->_lastState = 42;
+    ASSERT_EQ(42, base->read());
+}
+TEST_F(BaseTest, DISABLED_createClassWithName) {
+    unique_ptr<MockBaseInheritor> mockBase;
+    EXPECT_CALL(*mockBase, create(expectedName));
+    mockBase = unique_ptr<MockBaseInheritor>(new MockBaseInheritor());
+}
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,28 +1,28 @@
 #include <gtest/gtest.h>
-//#include <ddslinker/Input.h>
-//#include <memory>
-//
-//using namespace std;
-//using namespace Friend::DDS;
-//
+#include <ddslinker/Input.h>
+#include <memory>
+
+using namespace std;
+using namespace Friend::DDS;
+
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-//
-//class DDSTest : public ::testing::Test
-//{
-//protected:
-//    unique_ptr<Input<int>> linker;
-//    virtual void SetUp() override {
-//        linker = unique_ptr<Input<int>>(new Input<int>());
-//    }
-//    virtual void TearDown() override {
-//
-//    }
-//};
-//
-//TEST_F(DDSTest, classCreated) {
-//    ASSERT_TRUE(linker != nullptr);
-//}
+
+class DDSTest : public ::testing::Test
+{
+protected:
+    unique_ptr<Input<int>> linker;
+    virtual void SetUp() override {
+        linker = unique_ptr<Input<int>>(new Input<int>());
+    }
+    virtual void TearDown() override {
+
+    }
+};
+
+TEST_F(DDSTest, classCreated) {
+    ASSERT_TRUE(linker != nullptr);
+}
